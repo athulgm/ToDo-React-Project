7bba89752d478ad306b2fe7f44568b5d9d5c8fa7
athulgm <athulgmathew@gmail.com>
2023-02-06 14:41:23 +0000

    Commit ToDo project initially
    To-Do App created using React.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ToDoApp from './todo/Todo';
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <ToDoApp />
+    </div>
+  )
+};
+export default App
---
+++ src/todo/Todo.jsx
@@ -0,0 +1,150 @@
+import React, { useState } from 'react';
+
+function ToDoApp() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+  const [editIndex, setEditIndex] = useState(-1);
+
+  const addTodo = () => {
+    if (input.trim().length === 0) {
+      alert ("Enter a Task to add to the list");
+      return;
+    }
+    setTodos([...todos, input]);
+    setInput('');
+    setEditIndex(-1);
+  };
+  
+    const handleEdit = (index) => {
+    setInput(todos[index]);
+    setEditIndex(index);
+  };
+ 
+    const handleUpdate = () => {
+    const newTodos = [...todos];
+    newTodos[editIndex] = input;
+    setTodos(newTodos);
+    setInput('');
+    setEditIndex(-1);
+  };
+
+    const handleDelete = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+  return (
+    <>
+      <div className="main-container">
+        <h1>To-Do List</h1>
+        <div className="inputfield">
+          <input
+            type="text"
+            required
+            value={input}
+            maxLength={60}
+            placeholder="Enter task"
+            onChange={(event) => setInput(event.target.value)}
+          />
+          {editIndex === -1 ? (
+            <button onClick={addTodo}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            </button>
+          ) : (
+            <button onClick={handleUpdate}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 00-3.7-3.7 48.678 48.678 0 00-7.324 0 4.006 4.006 0 00-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3l-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 003.7 3.7 48.656 48.656 0 007.324 0 4.006 4.006 0 003.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3l-3 3"
+                />
+              </svg>
+            </button>
+          )}
+        </div>
+
+        <div className="outputtask">
+          {todos.map((todo, index) => (
+            <div className="taskmenu">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 40 26"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+                >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5"
+                />
+              </svg>
+              <p key={index}>
+                {todo}
+                <div className="buttonlist">
+                  <div className="button">
+                    <button onClick={() => handleEdit(index)}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                        />
+                      </svg>
+                    </button>
+                    <button onClick={() => handleDelete(index)}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="w-6 h-6"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ToDoApp;
